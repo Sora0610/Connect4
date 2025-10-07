91a2a7c7270dbc4e7d29dd08f1ee58cbe0df0ef9
Sorataro <sora.honda71@icloud.com>
2025-10-07 18:28:48 +0900

    Changed the board to hashmap

--- src/Connect4.java
+++ src/Connect4.java
@@ -1,31 +1,41 @@
 package src;
+import java.util.HashMap;
 
 public class Connect4 {
-    private char[][] board;
-    private boolean turns;
+    private int[][] board;
+    private int turns;
+    private HashMap<Integer, Character> symbols;
 
     public Connect4() {
-        board = new char[6][7];
-        turns = true;
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                board[i][j] = '□';
-            }
-        }
+        board = new int[6][7];
+        turns = 1;
+        symbols = new HashMap<>();
+        symbols.put(0, '□');
+        symbols.put(1, '●');
+        symbols.put(-1, '○');
     }
 
     //when player plays, input coordinate then update the board;
-    public void play(int x, int y, String player) {
-        if (board[x][y-1] == 0 || board[x][y] != 0) {
-            System.out.print("Invalid location");
+    public void play(int x, int y) {
+        // index out of bound when 1,1 , can place even if there's nothing under, can replace
+        if (x < 1 || x > 7 || y < 1 || y > 6) {
+            System.out.println("Invalid Input");
+            return;
         }
+
+        if((board[y-1][x-1] == 0 && board[y-2][x-1] != 0) || board[5][x-1] == 0){
+            board[y-1][x-1] = turns;
+        } else {
+            System.out.println("Invalid Input");
+        }
+        turns = -turns;
     }
 
     //print the board
     public void printBoard() {
-        for (int i = 0; i < 7; i++) {
-            for (int j = 0; j < 6; j++) {
-                System.out.print(board[i][j]);
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 7; j++) {
+                System.out.print(symbols.get(board[i][j]));
             }
             System.out.println();
         }
--- src/Main.java
+++ src/Main.java
@@ -6,15 +6,19 @@ public class Main {
     public static void main(String[] argc) {
         Scanner scan = new Scanner(System.in);
         Connect4 game = new Connect4();
-        
+
         // Get input
-        System.out.print("X: ");
-        int x = scan.nextInt();
-        System.out.print("Y: ");
-        int y = scan.nextInt();
+        while (true) {
+            // if () {
+            //     break;
+            // }
+            System.out.print("X: ");
+            int x = scan.nextInt();
+            System.out.print("Y: ");
+            int y = scan.nextInt();
+            game.play(x,y);
+        }
 
-
-
-        scan.close();
+        //scan.close();
     }
 }
