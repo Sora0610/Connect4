db12b285e4392b883ec09c005dffc1fde9397dfa
yjr28 <yjryu2424@gmail.com>
2025-10-07 02:22:23 +0000

    added play function in Connect4 class

---
+++ src/Connect4.java
@@ -0,0 +1,26 @@
+package src;
+
+public class Connect4 {
+    private String[][] board;
+    private boolean turns;
+
+    public Connect4() {
+        board = new String[6][7];
+        turns = true;
+    }
+
+    //when player plays, input coordinate then update the board;
+    public void play(int x, int y, String player) {
+        board[x][y] = player;
+    }
+
+    //print the board
+    public void printBoard() {
+        for (int i = 0; i < 7; i++) {
+            for (int j = 0; j < 6; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
