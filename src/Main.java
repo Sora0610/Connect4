72923e4e0dc30d8d69854e06ff6c6bae8b6b3299
Sorataro <sora.honda71@icloud.com>
2025-10-07 10:47:16 +0900

    adde connect4 class

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,14 @@ import java.lang.System;
 public class Main {
     public static void main(String[] argc) {
         Scanner scan = new Scanner(System.in);
-
+        Connect4 game = new Connect4();
+        
         // Get input
         System.out.print("X: ");
         int x = scan.nextInt();
         System.out.print("Y: ");
         int y = scan.nextInt();
+
 
 
         scan.close();
