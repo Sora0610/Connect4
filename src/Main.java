46bf40a5c8bcd5add7b6fc7990790faec6d6df74
Sorataro <sora.honda71@icloud.com>
2025-10-07 10:24:46 +0900

    Added System.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
 package src;
+import java.util.Scanner;
+import java.lang.System;
+
 public class Main {
     public static void main(String[] argc) {
-        blah blah blah
+        Scanner scan = new Scanner(System.in);
+
+        // Get input
+        System.out.print("X: ");
+        int x = scan.nextInt();
+        System.out.print("Y: ");
+        int y = scan.nextInt();
+
+
+        scan.close();
     }
 }
