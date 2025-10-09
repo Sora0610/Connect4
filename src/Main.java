4d21ce2e867928f4d57ac5f82f81d7ab2268aaac
Sorataro <sora.honda71@icloud.com>
2025-10-09 20:33:25 +0900

    added checkwinner and break conditions

--- src/Main.java
+++ src/Main.java
@@ -6,20 +6,21 @@ public class Main {
     public static void main(String[] argc) {
         Scanner scan = new Scanner(System.in);
         Connect4 game = new Connect4();
+        CheckWinner winner = new CheckWinner();
 
         // Get input
         while (true) {
-            // if () {
-            //     break;
-            // }
             System.out.print("X: ");
             int x = scan.nextInt();
             System.out.print("Y: ");
             int y = scan.nextInt();
             game.play(x,y);
             game.printBoard();
+            if (winner.checkBoard(game.returnBoard(), x, y)) {
+                break;
+            }
         }
 
-        //scan.close();
+        scan.close();
     }
 }
