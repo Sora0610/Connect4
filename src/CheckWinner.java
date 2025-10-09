e57ed471a654d1d48af3b67517b418c3c4315b55
Sorataro <sora.honda71@icloud.com>
2025-10-09 20:35:01 +0900

    added checkBoard, returnWinnerNo, and counter function

--- src/CheckWinner.java
+++ src/CheckWinner.java
@@ -1,5 +1,45 @@
 package src;
 
 public class CheckWinner {
-    
+    private static final int[][] direction = {{0, 1}, {1, 0}, {1, 1}, {-1, 1}}; //right, down, right down, right up
+    private static final int rowNumber = 6;
+    private static final int columnNumber = 7;
+
+    public boolean checkBoard(int[][] board, int x, int y) {
+        int row = y - 1;
+        int column = x - 1;
+        
+        int turns = board[row][column];
+
+        for (int[] a : direction) {
+            int count = 1 + counter(board, row, column, a[0], a[1], turns) + counter(board, row, column, -a[0], -a[1], turns);
+            if (count >= 4) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // return the winner number
+    public int returnWinnerNo(int[][] board, int lastRowMove, int lastColumnMove) {
+        if (checkBoard(board, lastRowMove, lastColumnMove)) {
+            return board[lastRowMove][lastColumnMove];
+        }
+        return 0;
+    }
+
+    public int counter(int[][] board, int row, int column, int rowDirection, int columnDirection, int turns) {
+        int count = 0;
+        int newRow = row + rowDirection;
+        int newColumn = column + columnDirection;
+
+        // while in the area of the board
+        while (newRow >= 0 && newRow < rowNumber && newColumn >= 0 && newColumn < columnNumber && board[newRow][newColumn] == turns) {
+            count++;
+            newRow += rowDirection;
+            newColumn += columnDirection;
+        }
+
+        return count;
+    }
 }
